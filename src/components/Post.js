ea4ebf792a8af48e3ae167bb30ebd4a76f892e66
isaacasher97 <isaac.asher97@gmail.com>
2023-09-25 23:59:20 -0400

    individual post defined

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,5 +1,19 @@
-function Post (props){
-    return <h1>Post</h1>
+import {Link} from "react-router-dom";
+
+function Post ({post}){
+
+    const div = {
+        texAlign: 'center',
+        border: '3px solid',
+        margin: '10px auto',
+        width: '80%'
+    }
+    return <div style={div}>
+        <Link to={`/post/${post.id}`}>
+            <h1>{post.name}</h1>
+            <h2>{post.calories} Calories</h2>
+        </Link>
+    </div>
 }
 
 export default Post;
