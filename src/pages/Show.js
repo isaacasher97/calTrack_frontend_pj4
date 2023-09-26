31c7e26ada1765db6c540166332f0b6b8c02c42d
isaacasher97 <isaac.asher97@gmail.com>
2023-09-26 00:21:22 -0400

    show page updated

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,5 +1,24 @@
+import { Link, useLoaderData } from "react-router-dom";
+
 function Show (props){
-    return <h1>Show</h1>
+    const post = useLoaderData()
+
+    const div = {
+        textAlign: "center",
+        border: "3px solid green",
+        width: "80%",
+        margin: "30px auto"
+    }
+
+    return <div style={div}>
+        <h1>{post.name}</h1>
+        <h2>{post.calories} Calories</h2>
+        <h3>{post.carbs}g Carbs</h3>
+        <h3>{post.fat}g Fat</h3>
+        <h3>{post.protein}g Protein</h3>
+        <h3>{post.sugar}g Sugar</h3>
+        <Link to="/">Back Home</Link>
+    </div>
 }
 
 export default Show;
