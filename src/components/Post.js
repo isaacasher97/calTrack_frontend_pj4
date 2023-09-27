a518c54b149ec9ee5a694b412416fa87801b06ce
isaacasher97 <isaac.asher97@gmail.com>
2023-09-27 00:05:45 -0400

    back to no auth

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,14 +1,7 @@
 import {Link} from "react-router-dom";
 
 function Post ({post}){
-
-    const div = {
-        texAlign: 'center',
-        border: '3px solid',
-        margin: '10px auto',
-        width: '80%'
-    }
-    return <div style={div}>
+    return <div className="post-compnt">
         <Link to={`/post/${post.id}`}>
             <h1>{post.name}</h1>
             <h2>{post.calories} Calories</h2>
--- src/router.js
+++ src/router.js
@@ -4,12 +4,10 @@ import Index from "./pages/Index"
 import Show from "./pages/Show"
 import { indexLoader,  showLoader } from "./loaders"
 import { createAction, updateAction, deleteAction } from "./actions"
-import Login from "./pages/Login"
-import getToken from "../src/App"
 
 const router = createBrowserRouter(createRoutesFromElements(
     <>
-        <Route path="/" element={<Login getToken={getToken} />}>
+        <Route path="/" element={<App/>}>
             <Route path="" element={<Index/>}loader={indexLoader}/>
             <Route path="post/:id" element={<Show/>} loader={showLoader}/>
             <Route path="create" action={createAction}/>
