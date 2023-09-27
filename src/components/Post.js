d96d2e206c8623b43b23f713735f6097b5ff4110
Isaac Asher <131828444+isaacasher97@users.noreply.github.com>
2023-09-26 23:37:47 -0400

    Revert "began sass styling"

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,7 +1,14 @@
 import {Link} from "react-router-dom";
 
 function Post ({post}){
-    return <div className="post-compnt">
+
+    const div = {
+        texAlign: 'center',
+        border: '3px solid',
+        margin: '10px auto',
+        width: '80%'
+    }
+    return <div style={div}>
         <Link to={`/post/${post.id}`}>
             <h1>{post.name}</h1>
             <h2>{post.calories} Calories</h2>
