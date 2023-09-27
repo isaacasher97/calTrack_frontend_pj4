59187eaaabf2f6d1355ee591c3c50c8a677cb756
isaacasher97 <isaac.asher97@gmail.com>
2023-09-26 23:39:27 -0400

    test

--- src/router.js
+++ src/router.js
@@ -4,10 +4,12 @@ import Index from "./pages/Index"
 import Show from "./pages/Show"
 import { indexLoader,  showLoader } from "./loaders"
 import { createAction, updateAction, deleteAction } from "./actions"
+import Login from "./pages/Login"
+import getToken from "../src/App"
 
 const router = createBrowserRouter(createRoutesFromElements(
     <>
-        <Route path="/" element={<App/>}>
+        <Route path="/" element={<Login getToken={getToken} />}>
             <Route path="" element={<Index/>}loader={indexLoader}/>
             <Route path="post/:id" element={<Show/>} loader={showLoader}/>
             <Route path="create" action={createAction}/>
