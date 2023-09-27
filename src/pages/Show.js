4208b2c401422d56af9c7b87ae4cae8d0b0f3066
isaacasher97 <isaac.asher97@gmail.com>
2023-09-27 04:23:26 -0400

    more sass styling added

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -2,16 +2,8 @@ import { Link, useLoaderData, Form } from "react-router-dom";
 
 function Show (props){
     const post = useLoaderData()
-
-    const div = {
-        textAlign: "center",
-        border: "3px solid green",
-        width: "80%",
-        margin: "30px auto"
-    }
-
     return (
-        <div style={div}>
+        <div className="update-form">
             <h1>{post.name}</h1>
             <h2>{post.calories} Calories</h2>
             <h3>{post.carbs}g Carbs</h3>
