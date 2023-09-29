74eb3501aca0ac9374e5929f7262d103f628b689
isaacasher97 <isaac.asher97@gmail.com>
2023-09-29 17:38:00 -0400

    footerBuild Complete

---
+++ src/components/DietLinks.js
@@ -0,0 +1,60 @@
+import React from 'react'
+
+const DietLinks = () => {
+  return (
+    <div id='various-diet-card-container' className="card text-bg-info mb-3">
+  <div className="card-header">
+    <h4 id='var-diet-plns-header'>
+        Various Diet Plans
+    </h4>
+    </div>
+  <div className="card-body">
+    <h4 className="card-title">
+        <a href='https://www.hopkinsmedicine.org/health/wellness-and-prevention/intermittent-fasting-what-is-it-and-how-does-it-work' target='_blank' rel='noreferrer'>
+        <span className='diet-link-icon'>
+            Intermittent Fasting
+        <svg className="svg-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-right-square" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.854 8.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707l-4.096 4.096z"/>
+        </svg>
+        </span>
+        </a>
+    </h4>
+    <p className="card-text">
+    Intermittent fasting means that you don't eat for a period of time each day or week. Some popular approaches to intermittent fasting include: Alternate-day fasting. Eat a normal diet one day and either completely fast or have one small meal (less than 500 calories) the next day. Click on the arrow above to read more.
+    </p>
+  </div>
+  <div className="card-body">
+    <h4 class="card-title">
+        <a href='https://www.hsph.harvard.edu/nutritionsource/healthy-weight/diet-reviews/ketogenic-diet/' target='_blank' rel='noreferrer'>
+        <span className='diet-link-icon'>
+            The Keto Diet
+        <svg className="svg-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-right-square" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.854 8.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707l-4.096 4.096z"/>
+        </svg>
+        </span>
+        </a>
+    </h4>
+    <p className="card-text">
+    The idea of the Keto diet (like the Atkins diet) is for you to get more calories from protein and fat and less from carbohydrates. You cut back most on the carbs that are easy to digest, like sugar, soda, pastries, and white bread. Click on the arrow above to read more.
+    </p>
+  </div>
+  <div className="card-body">
+    <h4 className="card-title">
+        <a href='https://health.usnews.com/wellness/food/articles/carnivore-diet-reviews-everything-you-need-to-know' target='_blank' rel='noreferrer'>
+        <span className='diet-link-icon'>
+            The Carnivore Diet
+        <svg className="svg-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-right-square" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.854 8.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707l-4.096 4.096z"/>
+        </svg>
+        </span>
+        </a>
+    </h4>
+    <p className="card-text">
+    The carnivore diet is a restrictive diet that only includes meat, fish, and other animal foods like eggs and certain dairy products. It excludes all other foods, including fruits, vegetables, legumes, grains, nuts, and seeds. Click on the arrow above to read more. 
+    </p>
+  </div>
+</div>
+  )
+}
+
+export default DietLinks
