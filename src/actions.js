ae6bedb6c05878936edd45a248417da9705dd2e6
isaacasher97 <isaac.asher97@gmail.com>
2023-09-30 10:25:50 -0400

    tuneup

--- src/actions.js
+++ src/actions.js
@@ -47,7 +47,7 @@ export const updateAction = async({request, params}) => {
     }
 
     // make a request to update a todo
-    await fetch(url + id, {
+    await fetch(url + id + '/', {
         method: "put",
         headers: {
             "Content-Type": "application/json"
