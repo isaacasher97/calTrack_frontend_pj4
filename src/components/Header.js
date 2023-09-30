308780266d08d3d2dbb9f405a79e5a56d69cb114
isaacasher97 <isaac.asher97@gmail.com>
2023-09-29 20:06:02 -0400

    updated sidebar

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,10 +22,6 @@ function Header(props) {
             <a className="nav-link" href="/">Logout</a>
           </li>
         </ul>
-        <form className="d-flex mt-3" role="search">
-          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-          <button className="btn btn-outline-success" type="submit">Search</button>
-        </form>
       </div>
     </div>
   </div>
